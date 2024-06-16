f0a0cf061a06876e63e41154b6a1495a1f62dc98
Ihor Belodedov <i.belodedov@rqc.ru>
2024-06-17 00:25:47 +0300

    back btn

--- src/UI/PageLayout/index.tsx
+++ src/UI/PageLayout/index.tsx
@@ -1,12 +1,30 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import TabBar from "../../components/TabBar";
 import styles from "./pageLayout.module.scss";
+import { useNavigate } from "react-router-dom";
+import { TELEGRAM } from "../../utils/constants";
 
 interface Props extends PropsWithChildren {
   withTabs?: boolean;
 }
 
 const PageLayout = ({ children, withTabs = false }: Props) => {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+  useEffect(() => {
+    if (window.history.length > 1) {
+      TELEGRAM.BackButton.show();
+      TELEGRAM.BackButton.onClick(goBack);
+    } else {
+      TELEGRAM.BackButton.hide();
+    }
+    return () => {
+      TELEGRAM.BackButton.offClick(goBack);
+    };
+  }, [window.history.length]);
+
   return (
     <div className={styles.container}>
       {children}
