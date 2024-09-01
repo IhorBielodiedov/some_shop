68cc2bde2c54f6e2505ef2b1d731a07804a4e85c
Ihor Belodedov <i.belodedov@rqc.ru>
2024-09-01 23:57:16 +0300

    restore old categories

--- src/UI/CategoryButton/index.tsx
+++ src/UI/CategoryButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, ForwardedRef, forwardRef } from "react";
+import { useEffect, useRef } from "react";
 import styles from "./categoryButton.module.scss";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -7,48 +7,33 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   big?: boolean;
   doFocus?: boolean;
 }
+const CategoryButton = ({
+  title,
+  active = false,
+  big = false,
+  doFocus = false,
+  ...attrs
+}: Props) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-const CategoryButton = forwardRef<HTMLButtonElement, Props>(
-  (
-    { title, active = false, big = false, doFocus = false, ...attrs },
-    ref: ForwardedRef<HTMLButtonElement>
-  ) => {
-    const buttonRef = useRef<HTMLButtonElement>(null);
+  useEffect(() => {
+    if (active && buttonRef.current && doFocus) {
+      buttonRef.current.scrollIntoView({
+        inline: "center",
+      });
+    }
+  }, [active]);
 
-    useEffect(() => {
-      if (active && buttonRef.current && doFocus) {
-        buttonRef.current.scrollIntoView({
-          inline: "center",
-        });
-      }
-    }, [active, doFocus]);
-
-    // Forward the ref to the button element
-    React.useImperativeHandle(ref, () => buttonRef.current!);
-
-    return (
-      <button
-        ref={(el) => {
-          // @ts-ignore
-          buttonRef.current = el;
-          if (typeof ref === "function") {
-            ref(el);
-          } else if (ref) {
-            (ref as React.MutableRefObject<HTMLButtonElement | null>).current =
-              el;
-          }
-        }}
-        className={`${big ? styles.big_button : styles.small_button} ${
-          active && styles.active
-        }`}
-        {...attrs}
-      >
-        <p className={`${styles.title} ${active && styles.active}`}>{title}</p>
-      </button>
-    );
-  }
-);
-
-CategoryButton.displayName = "CategoryButton";
-
+  return (
+    <button
+      ref={buttonRef}
+      className={`${big ? styles.big_button : styles.small_button} ${
+        active && styles.active
+      }`}
+      {...attrs}
+    >
+      <p className={`${styles.title} ${active && styles.active}`}>{title}</p>
+    </button>
+  );
+};
 export default CategoryButton;
--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,12 +1,8 @@
-import React, { useRef, useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import CategoryButton from "../../UI/CategoryButton";
 import { useProductsStore } from "../../stores/useProductsStore";
 import { Category } from "../../utils/types";
 
-import { FreeMode } from "swiper/modules";
 import styles from "./categories.module.scss";
-import "./swiper.scss";
 
 interface Props {
   list: Category[];
@@ -14,66 +10,30 @@ interface Props {
   wrap?: boolean;
 }
 
-const Categories: React.FC<Props> = ({ big = false, list, wrap = false }) => {
-  const [buttonWidths, setButtonWidths] = useState<number[]>([]);
+const Categories = ({ big = false, list, wrap = false }: Props) => {
   const activeCategory = useProductsStore((state) => state.activeCategory);
   const setActiveCategory = useProductsStore(
     (state) => state.setActiveCategory
   );
 
-  // Refs for CategoryButtons
-  const buttonRefs = useRef<(HTMLButtonElement | null)[]>([]);
-
-  // Update button widths when list changes
-  useEffect(() => {
-    const widths = buttonRefs.current.map((ref) => ref?.offsetWidth || 0);
-    setButtonWidths(widths);
-  }, [list]);
-
   return (
     <>
-      {!wrap ? (
-        <Swiper
-          slidesPerView={"auto"}
-          freeMode={true}
-          modules={[FreeMode]}
-          spaceBetween={4}
-          loop={true}
-          className="categoriesSwiper"
-        >
-          {list.map((item, index) => (
-            <SwiperSlide key={index} style={{ width: buttonWidths[index] }}>
-              <CategoryButton
-                title={item.alias ? item.alias : item.name}
-                active={activeCategory?.name === item.name}
-                big={big}
-                onClick={() => setActiveCategory(item)}
-                doFocus
-                ref={(el) => (buttonRefs.current[index] = el)}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      ) : (
-        <div
-          className={styles.container}
-          style={{ flexWrap: wrap ? "wrap" : "nowrap" }}
-        >
-          {list.map((item, index) => (
-            <CategoryButton
-              title={item.alias ? item.alias : item.name}
-              key={index}
-              active={activeCategory?.name === item.name}
-              big={big}
-              onClick={() => setActiveCategory(item)}
-              doFocus
-              ref={(el) => (buttonRefs.current[index] = el)}
-            />
-          ))}
-        </div>
-      )}
+      <div
+        className={styles.container}
+        style={{ flexWrap: wrap ? "wrap" : "nowrap" }}
+      >
+        {list.map((item, index) => (
+          <CategoryButton
+            title={item.alias ? item.alias : item.name}
+            key={index}
+            active={activeCategory?.name === item.name}
+            big={big}
+            onClick={() => setActiveCategory(item)}
+            doFocus
+          />
+        ))}
+      </div>
     </>
   );
 };
-
 export default Categories;
