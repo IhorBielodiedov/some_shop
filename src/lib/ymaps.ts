12965c48e93984633b87046212accd1b75906f13
Ihor Belodedov <i.belodedov@rqc.ru>
2024-12-01 16:37:56 +0300

    ymaps

--- src/lib/ymaps.ts
+++ src/lib/ymaps.ts
@@ -15,3 +15,11 @@ export const {
   YMapDefaultFeaturesLayer,
   YMapControls,
 } = reactify.module(ymaps3);
+
+// вот так тоже пробовал, это ведет к ошибке
+// No overload matches this call.
+//   The last overload gave the following error.
+//     Argument of type '"@yandex/ymaps3-default-ui-theme"' is not assignable to parameter of type '"@yandex/ymaps3-vuefy"'.ts(2769)
+// export const { YMapZoomControl } = reactify.module(
+//   await ymaps3.import("@yandex/ymaps3-default-ui-theme")
+// );
