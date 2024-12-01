cd43acb67322547d866bdaa2089cc86c4ca0d7a6
Ihor Belodedov <i.belodedov@rqc.ru>
2024-12-02 00:23:00 +0300

    city search page

--- src/pages/CitySearchPage/index.tsx
+++ src/pages/CitySearchPage/index.tsx
@@ -1,19 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchPanel from "../../components/SearchPanel";
 import { useOrderStore } from "../../stores/orderStore";
 import CityItem from "../../components/CityItem";
 import styles from "./citySearchPage.module.scss";
 import { useNavigate } from "react-router-dom";
+import { TELEGRAM } from "../../utils/constants";
 
 const CitySearchPage = () => {
   const [searchValue, setSearchValue] = useState("");
-  const client = useOrderStore((state) => state.client);
-  const getCities = useOrderStore((state) => state.getCitiesList);
   const cities = useOrderStore((state) => state.providerCities);
   const updateClientProperty = useOrderStore(
     (state) => state.updateClientProperty
   );
   const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+    if (window.history.length > 1) {
+      TELEGRAM.BackButton.show();
+      TELEGRAM.BackButton.onClick(goBack);
+    } else {
+      TELEGRAM.BackButton.hide();
+    }
+    return () => {
+      TELEGRAM.BackButton.offClick(goBack);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
