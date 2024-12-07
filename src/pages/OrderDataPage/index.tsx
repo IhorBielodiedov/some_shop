07e21dccf6f6848646c122223f634b5d42c80cf7
Ihor Belodedov <i.belodedov@rqc.ru>
2024-12-07 21:03:23 +0300

    points cdek

--- src/pages/OrderDataPage/index.tsx
+++ src/pages/OrderDataPage/index.tsx
@@ -17,6 +17,7 @@ export const OrderDataPage = () => {
   const client = useOrderStore((state) => state.client);
   const getCities = useOrderStore((state) => state.getCitiesList);
   const getPoints = useOrderStore((state) => state.getPointsList);
+  const points = useOrderStore((state) => state.points);
   const updateClientProperty = useOrderStore(
     (state) => state.updateClientProperty
   );
@@ -42,79 +43,7 @@ export const OrderDataPage = () => {
   }, []);
 
   const [indxesVisiblePlacemark, setVisiblePlacemark] = useState([]);
-  const points = {
-    rows: [
-      {
-        id: "521456",
-        providerKey: "cse",
-        type: 2,
-        availableOperation: 2,
-        cod: 1,
-        paymentCash: null,
-        paymentCard: 1,
-        name: "Почтомат 5post в Пятерочке OM103327",
-        lat: 56.3286537,
-        lng: 37.5211701,
-        code: "c4b96690-d4cc-11eb-80fe-0090faaaf8e4",
-        postIndex: "141804",
-        countryCode: "RU",
-        region: "Московская",
-        regionType: "обл",
-        area: null,
-        city: "Дмитров",
-        cityType: "г",
-        cityGuid: "0d8a246f-6446-403e-91c6-3aec9cdbfc32",
-        community: null,
-        communityType: null,
-        communityGuid: null,
-        street: "Веретенникова",
-        streetType: "ул",
-        house: "25",
-        address: "141804, обл Московская, г Дмитров, ул Веретенникова, дом 25",
-        block: null,
-        office: null,
-        url: null,
-        email: null,
-        phone: "88005118800",
-        timetable:
-          "ПН 08:01 - 23:01,ВТ 08:01 - 23:01,СР 08:01 - 23:01,ЧТ 08:01 - 23:01,ПТ 08:01 - 23:01,СБ 08:01 - 23:01,ВС 08:01 - 23:01",
-        worktime: {
-          "1": "08:01/23:01",
-          "2": "08:01/23:01",
-          "3": "08:01/23:01",
-          "4": "08:01/23:01",
-          "5": "08:01/23:01",
-          "6": "08:01/23:01",
-          "7": "08:01/23:01",
-        },
-        photos: null,
-        fittingRoom: 0,
-        description: null,
-        metro: null,
-        multiplaceDeliveryAllowed: null,
-        extra: [
-          {
-            key: "ownerCode",
-            value: "Почтомат",
-          },
-        ],
-        limits: {
-          maxSizeA: null,
-          maxSizeB: null,
-          maxSizeC: null,
-          maxSizeSum: null,
-          minWeight: null,
-          maxWeight: null,
-          maxCod: 100000,
-        },
-      },
-    ],
-    meta: {
-      total: 1,
-      offset: 0,
-      limit: 10,
-    },
-  };
+
   return (
     <div className={styles.container}>
       <div>
