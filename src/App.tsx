4cc5b0c79e68575fde24e93e3822649712fcf690
Ihor Belodedov <i.belodedov@rqc.ru>
2025-03-04 16:13:43 +0300

    token env var

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,7 @@ function App() {
   useEffect(() => {
     TELEGRAM.setBackgroundColor("#ffffff");
     TELEGRAM.expand();
-    localStorage.setItem(
-      "access",
-      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwic3ViIjoiMCIsInJvbGUiOiJqd3RfY2xpZW50Iiwic2siOiI3OTIzYzUzZi0xNDgyLTQ0MjgtODgyZC02ZjU3MTNlZWQ2NmUiLCJleHAiOjE3NDc2OTU3NjAuMTYyNzUzLCJpYXQiOjE3Mzk4MTE3NjAuMTYyNzUzfQ.FI66-uIVPQld6y5tacZrCJz7zGJyIeYj-LyS-glc35UpOErKOILjVY6ShK1RYB1mbu5MN_1PscKf_ZNq975Wqi-r2ksCkMHfq6MfFG10FlcE3o4TRIuz-8xmTOZq5DWi0SuMv9cbK3Tb02m1aZviFpxSBi2Mbq4UwfM8O-X0YZVtreMd9cS5HiVURCQg6ieIPjf_HTUoMPLFZcRCQrhqgbIsVc0F75UqtZCl8AtrJtk2lBKKX8skYYHwJ_aFbxwGwdgMv7tpSGptMQXfE-IiesM4fax3qzug8df5Xt0rn6A0AoQZ9hXhpON2o-OF2lVUikrIGlh_y5CSPJXR0jB-1Q"
-    );
+    localStorage.setItem("access", process.env.REACT_APP_API_TOKEN);
     // api.getCarts("1");
   }, []);
 
